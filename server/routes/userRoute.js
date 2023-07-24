e2028f65796f19c673030f46a1349831f4566ccd
Akash Koodali <akashkoodali9@gmail.com>
2023-07-24 20:34:21 +0530

    completed server

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,27 @@
 import express from "express";
-import { createUser } from "../controller/userCntrl.js";
+import {
+  createUser,
+  bookVisit,
+  getAllBookings,
+  toFav,
+  cancelBooking,
+  getAllFavorites,
+} from "../controller/userCntrl.js";
+
+// import jwtCheck from "../config/auth0Config.js";
 
 const router = express.Router();
 
 router.post("/register", createUser);
+
+router.post("/bookVisit/:id", bookVisit);
+
+router.post("/allBookings", getAllBookings);
+
+router.post("/removeBooking/:id", cancelBooking);
+
+router.post("/toFav/:rid", toFav);
+
+router.post("/allFav/", getAllFavorites);
 
 export { router as userRoute };
